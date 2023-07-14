c71a939421bffa05ba3a41cc26ffbe3a64f0683b
aaron <aaronwey@posteo.de>
2023-07-14 14:17:58 +0200

    Reworks training lua and enables different types of autocommands

--- training.lua
+++ training.lua
@@ -2,15 +2,21 @@ require("luarocks.loader")
 
 local absolute_line_task = require("tasks/absolute_line_task")
 local relative_line_task = require("tasks/relative_line_task")
-local buffer_permutation_task = require("tasks.buffer_permutation")
+local buffer_permutation_task = require("tasks/buffer_permutation")
+local test_task = require("tasks.test_task")
 local progress = require("progress")
 local status = require("status")
 
-local all_tasks = { absolute_line_task, relative_line_task }
-local all_tasks = { buffer_permutation_task }
+local all_tasks = { absolute_line_task, relative_line_task, buffer_permutation_task }
+all_tasks = { test_task }
 local chosen_task = all_tasks[math.random(#all_tasks)]
+local current_autocmds = {}
 
-local function main(autocmd_args)
+function main(autocmd_args)
+
+	task_status = chosen_task.check()
+
+
 	if chosen_task.check() then
 		progress.update_streak()
 	else
@@ -18,9 +24,23 @@ local function main(autocmd_args)
 	end
 	chosen_task.teardown()
 	chosen_task = all_tasks[math.random(#all_tasks)]
+	init_new_task()
+end
 
+function init_new_task()
 	chosen_task.init()
 	status.update(chosen_task.desc)
+
+	for i, v in pairs(current_autocmds) do
+		vim.api.nvim_del_autocmd(v)
+	end
+	current_autocmds = {}
+
+	for i, v in pairs(chosen_task.autocmds) do
+		current_autocmds[#current_autocmds + 1] = vim.api.nvim_create_autocmd({ v }, {
+			callback = main,
+		})
+	end
 end
 
 local function setup()
@@ -29,13 +49,7 @@ local function setup()
 	progress.init()
 	status.init()
 	vim.api.nvim_set_current_win(current_window)
-
-	chosen_task.init()
-	status.update(chosen_task.desc)
-
-	--vim.api.nvim_create_autocmd({"CursorMoved"}, {
-	--	callback = main,
-	--})
+	init_new_task()
 end
 
 setup()
