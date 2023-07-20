07c0d020d014ec0a497a538dcf5f471e0a6fc39b
aaron <aaronwey@posteo.de>
2023-07-20 15:28:47 +0200

    Fixes an issue with absolute line task

--- src/tasks/absolute_line_task.lua
+++ src/tasks/absolute_line_task.lua
@@ -1,7 +1,5 @@
 local AbsoluteLineTask = {}
 
-local data = { target_line = 0, highlight_namespace = nil }
-
 local Task = require("src.task")
 
 function AbsoluteLineTask:new()
@@ -9,20 +7,20 @@ function AbsoluteLineTask:new()
 	self.__index = self
 	setmetatable(newObj, self)
 
-	data.target_line = math.random(1, 15)
-	newObj.desc = "Move to line " .. tostring(data.target_line)
-	data.highlight_namespace = vim.api.nvim_create_namespace("AbsoluteVerticalLineNameSpace")
+	newObj.target_line = math.random(1, 15)
+	newObj.desc = "Move to line " .. tostring(newObj.target_line)
+	newObj.highlight_namespace = vim.api.nvim_create_namespace("AbsoluteVerticalLineNameSpace")
 
 	vim.api.nvim_set_hl(0, "UnderScore", { underline = true })
 
-	vim.api.nvim_buf_add_highlight(0, data.highlight_namespace, "UnderScore", data.target_line - 1, 0, -1)
+	vim.api.nvim_buf_add_highlight(0, newObj.highlight_namespace, "UnderScore", newObj.target_line - 1, 0, -1)
 
 	return newObj
 end
 
 function AbsoluteLineTask:completed()
 	local cursor_position = vim.api.nvim_win_get_cursor(0)[1]
-	local comparison = cursor_position == data.target_line
+	local comparison = cursor_position == self.target_line
 	return comparison
 end
 
@@ -31,8 +29,8 @@ function AbsoluteLineTask:failed()
 end
 
 function AbsoluteLineTask:teardown()
-	if data.highlight_namespace then
-		vim.api.nvim_buf_clear_namespace(0, data.highlight_namespace, 0, -1)
+	if self.highlight_namespace then
+		vim.api.nvim_buf_clear_namespace(0, self.highlight_namespace, 0, -1)
 	end
 end
 
