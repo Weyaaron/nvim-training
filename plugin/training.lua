f64cf20eefff1780ca6acd09da1a2fda7495dcbe
aaron <aaronwey@posteo.de>
2023-07-27 12:10:50 +0200

    Improves the startup by discarding a wrong cursor movement

--- plugin/training.lua
+++ plugin/training.lua
@@ -12,6 +12,7 @@ if not imports_succesfull then
 	--Fixes runtimepath for local development
 	vim.cmd(":set runtimepath=/home/aaron/Code/Lua/nvim_training")
 	vim.api.nvim_command("set runtimepath^=/home/aaron/Code/Lua/nvim_training")
+	vim.cmd(":ASToggle")
 end
 
 local UserInterFace = require("nvim_training.user_interface")
@@ -20,6 +21,15 @@ local current_autocmds = {}
 local Task_sequence = require("nvim_training.task_sequence")
 local current_task_sequence = Task_sequence:new()
 local user_interface
+
+local discard_movement = true
+
+function outer_loop()
+	if not discard_movement then
+		loop()
+	end
+	discard_movement = false
+end
 
 local function switch_to_next_task()
 	for _, autocmd_el in pairs(current_autocmds) do
@@ -31,13 +41,13 @@ local function switch_to_next_task()
 
 	for _, autocmd_el in pairs(current_task_sequence.current_task.autocmds) do
 		current_autocmds[#current_autocmds + 1] = vim.api.nvim_create_autocmd({ autocmd_el }, {
-			callback = main,
+			callback = outer_loop,
 		})
 	end
 	user_interface:display(current_task_sequence)
 end
 
-function main(autocmd_args)
+function loop()
 	local completed = current_task_sequence.current_task:completed()
 	local failed = current_task_sequence.current_task:failed()
 	if completed and not failed then
@@ -51,16 +61,30 @@ function main(autocmd_args)
 	if failed and completed then
 		print("A Task should not both complete and fail!")
 	end
+	if current_task_sequence.task_length == current_task_sequence.task_index then
+		current_task_sequence = Task_sequence:new()
+	end
 
 	user_interface:display(current_task_sequence)
 end
+
 
 function setup()
 	local current_window = vim.api.nvim_tabpage_get_win(0)
 
 	user_interface = UserInterFace:new()
 	vim.api.nvim_set_current_win(current_window)
-	switch_to_next_task()
+
+	current_task_sequence.current_task = current_task_sequence.task_sequence[1]
+	current_task_sequence.current_task:prepare()
+
+	for _, autocmd_el in pairs(current_task_sequence.current_task.autocmds) do
+		current_autocmds[#current_autocmds + 1] = vim.api.nvim_create_autocmd({ autocmd_el }, {
+			callback = outer_loop,
+		})
+	end
+
+	user_interface:display(current_task_sequence)
 end
 
 vim.api.nvim_create_user_command("Training", setup, {})
