8667851049f02e43bf107802d8fbd6f318674775
aaron <aaronwey@posteo.de>
2023-07-27 09:33:15 +0200

    Fixes the highlight in relative line and adds a fixed buffer to this task

--- lua/nvim_training/tasks/relative_line.lua
+++ lua/nvim_training/tasks/relative_line.lua
@@ -7,39 +7,36 @@ RelativeLineTask.base_args = { tags = { "movement", "relative" } }
 function RelativeLineTask:prepare()
 	self:load_from_json("relative_line.buffer")
 	self.previous_line = 0
-	self.target_offset = 0
 
-	local current_offset = utility.draw_random_number_with_sign(2, 9)
+	self.current_offset = utility.draw_random_number_with_sign(2, 9)
 	self.previous_line = vim.api.nvim_win_get_cursor(0)[1]
 
-	while current_offset + self.previous_line < 0 do
-		current_offset = utility.draw_random_number_with_sign(2, 9)
+	while self.current_offset + self.previous_line < 0 do
+		self.current_offset = utility.draw_random_number_with_sign(2, 9)
 	end
 
-	self.desc = "Move " .. tostring(current_offset) .. " lines relative to your cursor."
+	self.desc = "Move " .. tostring(self.current_offset) .. " lines relative to your cursor."
 
 	self.highlight_namespace = vim.api.nvim_create_namespace("RelativeVerticalLineNameSpace")
 
 	vim.api.nvim_set_hl(0, "UnderScore", { underline = true })
 
-	--Todo: Fix this weird highlight, in particular its index
-	vim.api.nvim_buf_add_highlight(0, self.highlight_namespace, "UnderScore", 10, 0, -1)
-	self.target_offset = current_offset
+	local line_for_highlight = self.previous_line + self.current_offset - 1
+	vim.api.nvim_buf_add_highlight(0, self.highlight_namespace, "UnderScore", line_for_highlight, 0, -1)
 end
 
-function RelativeLineTask:teardown()
-	if self.highlight_namespace then
-		vim.api.nvim_buf_clear_namespace(0, self.highlight_namespace, 0, -1)
-	end
+function RelativeLineTask:completed()
+	return vim.api.nvim_win_get_cursor(0)[1] == self.previous_line + self.current_offset
 end
 
 function RelativeLineTask:failed()
 	return not self:completed()
 end
 
-function RelativeLineTask:completed()
-	local target_line = self.previous_line + self.target_offset
-	return vim.api.nvim_win_get_cursor(0)[1] == target_line
+function RelativeLineTask:teardown()
+	if self.highlight_namespace then
+		vim.api.nvim_buf_clear_namespace(0, self.highlight_namespace, 0, -1)
+	end
 end
 
 return RelativeLineTask
