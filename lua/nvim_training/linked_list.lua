5aa6b6d83b917a41d8238878110c1b5e691ae904
aaron <aaronwey@posteo.de>
2023-08-20 10:43:25 +0200

    Improves the linked list by adding a few movements

--- lua/nvim_training/linked_list.lua
+++ lua/nvim_training/linked_list.lua
@@ -13,8 +13,7 @@ function LinkedListNode:new(content, line_index, start_index, end_index)
 	base.previous = nil
 	return base
 end
-
-function LinkedListNode:stringify()
+function LinkedListNode:_stringify()
 	local desc = self.content .. "(" .. self.line_index .. "," .. self.start_index .. "," .. self.end_index .. ")"
 	if not self.content then
 		return "empty content"
@@ -26,7 +25,11 @@ function LinkedListNode:stringify()
 	if self.next == nil then
 		return desc
 	end
-	return desc .. " " .. self.next:stringify()
+	return desc .. " " .. self.next:_stringify()
+end
+
+function LinkedListNode:stringify()
+	return self:root():_stringify()
 end
 
 function LinkedListNode:traverse(condition_fn)
@@ -41,17 +44,19 @@ function LinkedListNode:traverse(condition_fn)
 end
 
 function LinkedListNode:backtrack(condition_fn)
-	local current_node = condition_fn(self)
-	if current_node then
+	local current_status = condition_fn(self)
+	if current_status then
 		return self
 	end
 	if self.previous then
 		return self.previous:backtrack(condition_fn)
+	else
+		return self
 	end
-	return nil
 end
 
 function LinkedListNode:traverse_to_line_char(line_index, char_index)
+	--Todo: Can we fix this?
 	char_index = char_index + 1
 
 	local function traversal_function(input_node)
@@ -74,19 +79,39 @@ function LinkedListNode:traverse_to_line_char(line_index, char_index)
 end
 
 function LinkedListNode:traverse_and_consume(stop_func)
+	--Do I do this properly? Test it!
 	local current_status = stop_func(self)
 	if not current_status then
 		if self.previous then
 			self.previous.next = self.next
 		end
-		return self.next:traverse_and_consume(self.next, stop_func)
+		if self.next then
+			self.next.previous = self.previous
+		end
+		return self.next:traverse_and_consume(stop_func)
 	else
 		return self
 	end
 end
+
+function LinkedListNode:consume_up_until_node(target_node)
+	local function cmp_func(input_node)
+		return input_node.content == target_node.content
+	end
+	return self:traverse_and_consume(cmp_func)
+end
+
+function LinkedListNode:root()
+	local function _end(input_node)
+		return input_node == nil
+	end
+	return self:backtrack(_end)
+end
+
 function LinkedListNode:traverse_n(distance)
 	local counter = 0
 	local function inner_traverse(input_node)
+		--print("Traversing " ..input_node.content.. "with" ..counter )
 		local comparison = counter == distance
 		counter = counter + 1
 		return comparison
@@ -94,10 +119,63 @@ function LinkedListNode:traverse_n(distance)
 	return self:traverse(inner_traverse)
 end
 
-
-
-
-
-
+function LinkedListNode:w(offset)
+	return self:traverse_n(offset)
+end
+function LinkedListNode:test()
+	return { self.next }
+end
+function LinkedListNode.e(cursor_x, cursor_y, offset)
+	--Todo: Implement this!
+	local cursor_pos = vim.api.nvim_win_get_cursor(0)
+	local cursor_node = self.buffer_as_list:traverse_to_line_char(cursor_pos[1], cursor_pos[2])
+	local target_node = cursor_node:traverse_n(self.offset)
+	return { target_node.line_index, target_node.end_index - 2 }
+end
 
 return LinkedListNode
+--[[
+function LinkedListNode.f(input_node, cursor_x, cursor_y, custom_args)
+	local cursor_node = self.buffer_as_list:traverse(utility.traverse_to_cursor)
+
+	local function traverse_to_char(input_node)
+		local search_result = utility.search_for_char_in_word(input_node.content, self.target_char)
+		return not (search_result == -1)
+	end
+
+	local final_node = cursor_node:traverse(traverse_to_char)
+	local char_offset_in_word = utility.search_for_char_in_word(final_node.content, self.target_char)
+
+	--Todo: Evaluate the -2 and fix it
+	return { final_node.line_index, final_node.start_index + char_offset_in_word - 2 }
+end
+
+function Movements.search(input_node, cursor_x, cursor_y, custom_args)
+	local cursor_position = vim.api.nvim_win_get_cursor(0)
+	local cursor_node = self.buffer_as_list:traverse_to_line_char(cursor_position[1], cursor_position[2])
+
+	local target_node = cursor_node:traverse_n(self.offset)
+
+	return { target_node.line_index, target_node.start_index - 1 }
+end
+
+
+function tMovement:_execute_calculation()
+	local cursor_pos = vim.api.nvim_win_get_cursor(0)
+	local cursor_pos_x = cursor_pos[1]
+	local cursor_pos_y = cursor_pos[2]
+	local y_target = 0
+	local max_line_len = #self.current_line
+	for i = cursor_pos_y + 2, max_line_len, 1 do
+		local current_char = string.sub(self.current_line, i, i)
+		if current_char == self.target_char then
+			y_target = i - 1
+			break
+		end
+	end
+
+	return { cursor_pos_x, y_target - 1 }
+end
+
+]]
+--
