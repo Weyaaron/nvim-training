506e6c72a788e236aad9dd1b10ebe435a92f445f
aaron <aaronwey@posteo.de>
2023-08-20 10:42:20 +0200

    Deletes the old movement code

--- lua/nvim_training/movements/search_movement.lua
+++
@@ -1,9 +0,0 @@
-local base_movement = require("lua/nvim_training/movements/base_movement")
-local SearchMovement = base_movement:new()
-SearchMovement.__index = SearchMovement
-
-SearchMovement.base_args = { offset = 0 }
-
-function SearchMovement:_execute_calculation() end
-
-return SearchMovement
