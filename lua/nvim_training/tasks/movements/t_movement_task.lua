5ed33ab814263e1abddfe05036269bffd16b31e9
aaron <aaronwey@posteo.de>
2023-08-22 15:00:22 +0200

    I am stuck on this t task

--- lua/nvim_training/tasks/movements/t_movement_task.lua
+++ lua/nvim_training/tasks/movements/t_movement_task.lua
@@ -1,7 +1,6 @@
 -- luacheck: globals vim
 
 local Task = require("nvim_training.task")
-local tMovement = require("lua.nvim_training.movements.t_movement")
 
 local tMovementTask = Task:new()
 tMovementTask.base_args = { autocmds = { "CursorMoved" }, tags = { "buffer" } }
@@ -11,22 +10,30 @@ function tMovementTask:prepare()
 	self:load_from_json("permutation.buffer")
 	utility.replace_main_buffer_with_str(self.initial_buffer)
 
+
 	local current_cursor = vim.api.nvim_win_get_cursor(0)
-	local current_buffer_lines = vim.api.nvim_buf_get_lines(0, current_cursor[1], current_cursor[1] + 1, false)
-	local current_cursor_line = current_buffer_lines[1]
+	local current_line = vim.api.nvim_buf_get_lines(0, current_cursor[1], current_cursor[1] + 1, false)[1]
+	local current_line_as_list = utility.construct_linked_list(current_line)
+	--Doing this no sucks  matter which way ...
+
 	local left_sub_str = string.sub(current_cursor_line, current_cursor[2], #current_cursor_line)
 	local possible_chars = utility.generate_char_set(left_sub_str)
-
 	local target_char = possible_chars[math.random(#possible_chars)]
 
-	self.movement = tMovement:new()
-	self.movement.target_char = target_char
 
-	self.new_buffer_coordinates = self.movement:calculate_cursor_x_y()
+
+
+	local cursor_node = self.buffer_as_list:traverse_to_line_char(current_cursor[1], current_cursor[2])
+	local target_node = cursor_node:t(target_char)
 
 	self.desc = "Jump before the next instance of " .. target_char .. "."
 
-	self.highlight = utility.create_highlights(self.new_buffer_coordinates[1] - 1, self.new_buffer_coordinates[2], 1)
+	self.new_buffer_coordinates = {
+		target_node.line_index,
+		target_node.start_index + utility.search_for_char_in_word(target_node.content, target_char) - 1,
+	}
+
+	self.highlight = utility.create_highlight(self.new_buffer_coordinates[1] - 1, self.new_buffer_coordinates[2], 2)
 end
 
 function tMovementTask:completed()
@@ -45,3 +52,16 @@ function tMovementTask:teardown()
 end
 
 return tMovementTask
+--[[
+
+
+	local current_cursor = vim.api.nvim_win_get_cursor(0)
+
+	local cursor_node = self.buffer_as_list:traverse_to_line_char(current_cursor[1], current_cursor[2])
+	local offset = math.random(3,12)
+
+	local word_source_node = cursor_node:traverse_n(offset)
+	local word_offset = math.random(1, #word_source_node.content)
+	local target_char = string.sub(word_source_node.content, word_offset, word_offset)
+	print("target_char is:" .. target_char)
+--]]
