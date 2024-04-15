b688709c374f5a94bf89e1e6e72c6334b8f621cf
Mert Övün <mrtovun@gmail.com>
2024-04-15 19:12:27 +0300

    Yank teardown success criterion with clipboard comparison

--- lua/nvim-training/tasks/yank.lua
+++ lua/nvim-training/tasks/yank.lua
@@ -9,9 +9,13 @@ function YankTask:new()
 	return base
 end
 function YankTask:teardown(autocmd_callback_data)
-	local event_data = vim.deepcopy(vim.v.event)
+	-- local event_data = vim.deepcopy(vim.v.event)
 	-- print(self.target_text, "--", event_data.regcontents[1])
-	return event_data.regcontents[1] == self.target_text
+	local clipboard_content = vim.fn.getreg('"')
+
+	local yank_success = self.target_text == clipboard_content
+	return yank_success
+	-- return event_data.regcontents[1] == self.target_text
 end
 
 return YankTask
