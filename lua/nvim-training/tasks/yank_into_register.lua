a2816353f2d23d42c2bbf6d9739bc33f54a71dc1
Mert Övün <mrtovun@gmail.com>
2024-04-15 21:53:51 +0300

    YankIntoRegisterTask inherits from YankTask

--- lua/nvim-training/tasks/yank_into_register.lua
+++ lua/nvim-training/tasks/yank_into_register.lua
@@ -1,18 +1,19 @@
-local Task = require("nvim-training.task")
 local utility = require("nvim-training.utility")
 local user_config = require("nvim-training.user_config")
+local YankTask = require("nvim-training.tasks.yank")
 
-local YankIntoRegisterTask = {}
+local YankIntoRegisterTask = YankTask:new()
 YankIntoRegisterTask.__index = YankIntoRegisterTask
 
-function YankIntoRegisterTask:setup()
-	local base = Task:new()
+function YankIntoRegisterTask:new()
+	local base = YankTask:new()
 	setmetatable(base, { __index = YankIntoRegisterTask })
 
-	self.target_line = 0
+	-- self.target_line = 0
 	self.autocmd = "TextYankPost"
 	self.possible_registers = user_config.possible_register_list
-	self.choosen_register = self.possible_registers[#self.possible_register_list]
+	self.chosen_register = self.possible_registers[#self.possible_registers]
+	
 	local function _inner_update()
 		utility.set_buffer_to_lorem_ipsum_and_place_cursor_randomly()
 
@@ -21,18 +22,15 @@ function YankIntoRegisterTask:setup()
 		local lines = vim.api.nvim_buf_get_lines(0, cursor_pos[1] - 1, cursor_pos[1], false)
 		local line_length = #lines[1]
 		self.highlight = utility.create_highlight(cursor_pos[1] - 1, cursor_pos[2], line_length)
+		
+		self.target_text = lines[1] .. "\n"
 	end
 	vim.schedule_wrap(_inner_update)()
 	return base
 end
 
-function YankIntoRegisterTask:teardown(autocmd_callback_data)
-	utility.clear_highlight(self.highlight)
-	return false
-end
-
 function YankIntoRegisterTask:description()
-	return "Copy the line the cursor is in into register " .. self.choosen_register
+	return "Copy the line the cursor is in into register " .. self.chosen_register
 end
 
 return YankIntoRegisterTask
