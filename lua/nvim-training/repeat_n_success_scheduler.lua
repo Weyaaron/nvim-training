4be45a96964e4d9b53beec87aa454daecbcd83ee
aaron <aaronwey@posteo.de>
2024-07-17 14:01:39 +0200

    Scheduling: Adds until-n-successes scheduler
    This commit enhances the available schedulers by
    implementing a scheduler that repeats the
    same task until n successes have been achieved.

---
+++ lua/nvim-training/repeat_n_success_scheduler.lua
@@ -0,0 +1,40 @@
+local TaskScheduler = require("nvim-training.task_scheduler")
+
+RepeatNSuccessScheduler = TaskScheduler:new()
+
+function RepeatNSuccessScheduler:new(task_collections, kwargs)
+	local default_kwargs = {}
+	if not kwargs then
+		kwargs = default_kwargs
+	end
+
+	local base = TaskScheduler:new(task_collections, kwargs)
+	setmetatable(base, { __index = RepeatNSuccessScheduler })
+
+	--Todo: Add header extension for schedulers
+
+	base.success_limit = 5
+	base.successes = 0
+	base.task_counter = 1
+	base.all_task = {}
+	for i, task_collection_el in pairs(task_collections) do
+		for ii, task_el in pairs(task_collection_el.tasks) do
+			base.all_task[#base.all_task + 1] = task_el
+		end
+	end
+	return base
+end
+
+function RepeatNSuccessScheduler:next(previous, result)
+	if result then
+		self.successes = self.successes + 1
+	end
+	if self.successes == self.success_limit then
+		self.successes = 0
+		self.task_counter = self.task_counter + 1
+	end
+
+	return self.all_task[self.task_counter % #self.all_task + 1]
+end
+
+return RepeatNSuccessScheduler
--- lua/nvim-training/scheduler_index.lua
+++ lua/nvim-training/scheduler_index.lua
@@ -1,5 +1,6 @@
 local exported_schedulers = {
 	RandomScheduler = require("nvim-training.random_scheduler"),
+	RepeatNSuccessScheduler = require("nvim-training.repeat_n_success_scheduler"),
 }
 
 return exported_schedulers
