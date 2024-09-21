c459a616c5f35213bc68dce4c0f1aa4031bae5df
Aaron <aaronwey@posteo.de>
2024-09-21 08:44:13 +0200

    Fix: Issue with startup-checks
    The previous startup check had issues when events where disabled.
    This commit adresses these and refactors some related code.

--- lua/nvim-training/init.lua
+++ lua/nvim-training/init.lua
@@ -2,15 +2,12 @@ local user_config = require("nvim-training.user_config")
 local utility = require("nvim-training.utility")
 local module = {}
 
-function module.check_path(base_path)
-	return user_config.enable_events and utility.exists(base_path)
-end
 
 local function configure(args)
 	for i, v in pairs(args) do
 		user_config[i] = v
 	end
-	if not module.check_path(user_config.base_path) then
+	if not utility.exists(user_config.base_path) then
 		print(
 			tostring(user_config.base_path)
 				.. " does not exist. Creation will be attempted! If this path does not suit you, use 'configure' to set it."
