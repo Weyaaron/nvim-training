1e719b154adb4abb81e8c4cbcc5e2df686f0647e
Aaron <aaronwey@posteo.de>
2025-01-27 11:39:09 +0100

    Chore: Adds warning on unsupported config
    The config values may change over time.
    The new configure method contains code
    that warns users about unsupported/old
    arguments.

--- lua/nvim-training/init.lua
+++ lua/nvim-training/init.lua
@@ -2,15 +2,25 @@ local user_config = require("nvim-training.user_config")
 local utility = require("nvim-training.utility")
 local module = {}
 
+local function generate_out_str(middle)
+	return "The config parameter " .. middle .. " is not supported. To ensure a smooth experience consider changing/removing it."
+end
+
 local function configure(args)
 	for i, v in pairs(args) do
-		if not (type(i) == "table") then
-			user_config[i] = v
-		end
-		if type(i) == "table" then
-			for ii, vv in pairs(i) do
+		if type(v) == "table" and string.find(i, "_args") then
+			for ii, vv in pairs(v) do
+				if not user_config[i][ii] then
+					print(generate_out_str(i .. ":" .. ii))
+				end
+
 				user_config[i][ii] = vv
 			end
+		else
+			if not user_config[i] then
+				print(print(generate_out_str(i)))
+			end
+			user_config[i] = v
 		end
 	end
 end
