9e67b72c45e263af9aa623e0e97654ac0206c88d
Aaron <aaronwey@posteo.de>
2025-02-05 14:33:59 +0100

    Scheduling: Fixes the RepeatNScheduler
    The RepeatNScheduler had various issues,
    this is a rework of the underlying code.

--- lua/nvim-training/schedulers/repeat_n_scheduler.lua
+++ lua/nvim-training/schedulers/repeat_n_scheduler.lua
@@ -1,5 +1,6 @@
 local TaskScheduler = require("nvim-training.task_scheduler")
 local user_config = require("nvim-training.user_config")
+local task_index = require("nvim-training.task_index")
 
 RepeatNScheduler = {}
 RepeatNScheduler.__index = RepeatNScheduler
@@ -9,28 +10,30 @@ function RepeatNScheduler:new(task_collections)
 	local base = TaskScheduler:new(task_collections)
 	setmetatable(base, { __index = RepeatNScheduler })
 
-	base.current_task_count = 0
-	base.task_counter = 1
-	base.all_task = {}
+	base.task_counter = 0
+	base.full_task_name_list = {}
 	for i, task_collection_el in pairs(task_collections) do
 		for ii, task_el in pairs(task_collection_el.tasks) do
-			base.all_task[#base.all_task + 1] = task_el
+			for iii = 1, user_config.scheduler_args.repetitions do
+				base.full_task_name_list[#base.full_task_name_list + 1] = task_el.name
+			end
 		end
 	end
-	table.sort(base.all_task, function(a, b)
-		return a.name < b.name
+
+	-- This sort ensures that tasks like MoveT,MoveT stay reasonably sorted
+	table.sort(base.full_task_name_list, function(a, b)
+		if string.lower(a) == string.lower(b) then
+			return a < b
+		end
+		return string.lower(a) < string.lower(b)
 	end)
 	return base
 end
 
 function RepeatNScheduler:next(previous, result)
-	self.current_task_count = self.current_task_count + 1
-	if self.current_task_count == user_config.scheduler_args.repetitions then
-		self.current_task_count = 0
-		self.task_counter = self.task_counter + 1
-	end
-
-	return self.all_task[self.task_counter % #self.all_task + 1]
+	self.task_counter = self.task_counter + 1
+	local choosen_task = task_index[self.full_task_name_list[self.task_counter]]
+	return choosen_task
 end
 
 return RepeatNScheduler
